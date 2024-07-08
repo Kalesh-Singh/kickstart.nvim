dc51050bfd411dd267c3e7ff5962afc540a6ca8b
Kalesh-Singh <kaleshsingh96@gmail.com>
2024-07-07 22:53:31 -0700

    Add aerial plugin

--- init.lua
+++ init.lua
@@ -258,6 +258,26 @@ require('lazy').setup({
     },
   },
 
+  -- Aerial gives outline of code structure for quickly browsing code.
+  {
+    'stevearc/aerial.nvim',
+    opts = {
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set('n', '<C-p>', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', '<C-n>', '<cmd>AerialNext<CR>', { buffer = bufnr })
+      end,
+    },
+    -- Optional dependencies
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+
+    -- You probably also want to set a keymap to toggle aerial
+    vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>'),
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
